21f56ece6137b9cdd92834f0a87cb0f48c23bda2
srikantheddula <you@example.com>
2022-02-03 10:32:38 +0530

    [srikanth]: adding Uc1 in Tictactoe

---
+++ TicTac/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TicTac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TicTacToeGame tictac = new TicTacToeGame();
+            tictac.Board();
+                
+        }
+    }
+}
---
+++ TicTac/TicTacToeGame.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTac
+{
+    internal class TicTacToeGame
+    {
+        
+
+        public void Board()
+        {
+            string [] arr = new string [10];
+            arr[0] = "";
+            arr[1] = "";
+            arr[2] = "";
+            arr[3] = "";
+            arr[4] = "";
+            arr[5] = "";
+            arr[6] = "";
+            arr[7] = "";
+            arr[8] = "";
+            arr[9] = "";
+            arr[10] = "";
+
+        }
+    }
+}
